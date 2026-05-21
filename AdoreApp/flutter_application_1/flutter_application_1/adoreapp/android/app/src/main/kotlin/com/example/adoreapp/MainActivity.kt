c45bbaa9ca16664fc69dcbdf0ba74e3735e06f55
Seu Nome <seu_email_aqui@gmail.com>
2026-05-21 00:21:56 -0300

    chore: force delete AdoreApp remote

--- AdoreApp/flutter_application_1/flutter_application_1/adoreapp/android/app/src/main/kotlin/com/example/adoreapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.adoreapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- AdoreApp/flutter_application_1/flutter_application_1/adoreapp/flutter_application_1/nome_do_projeto/android/app/src/main/kotlin/com/example/nome_do_projeto/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nome_do_projeto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
